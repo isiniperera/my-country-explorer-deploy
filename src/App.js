72b3b0305fa4db5745b910daeaf737dc305796a9
isiniperera <IT22250124@my.sliit.lk>
2025-05-03 13:21:17 +0530

    updatr route

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// App.jsx
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Favorites from "./pages/Favourites";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
