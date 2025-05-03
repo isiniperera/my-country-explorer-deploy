5daab0d67be00c85040119becf8a6f5d7a2fc338
isiniperera <IT22250124@my.sliit.lk>
2025-05-03 13:19:17 +0530

    completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
-  
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: 'class', // ← ADD THIS LINE
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
